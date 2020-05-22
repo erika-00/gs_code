06363809b4d7ec79339e6265c984483f837df0f6
erika <a1126cayk@outlook.jp>
2020-05-22 23:21:23 +0900

    第三回課題

--- php01/post_confirm.php
+++ php01/post_confirm.php
@@ -1,23 +1,33 @@
 <?php
-$flg = 1;
+function h($value){
+ return htmlspecialchars($value, ENT_QUOTES);
+}
+$flg = 0;
 $name = $_POST["name"];
 $mail = $_POST["mail"];
-if($name==""){
-    $name = "未入力です";
-}
-if($mail==""){
-    $mail = "未入力です";
-}
 
+
+$ary = array("$name","$mail");
+$str = implode(',',$ary);
+
+var_dump($str);
+
+//File書き込み
+//課題　名前とメールの変数をカンマ区切りの文字列にしてファイルに書き込めるようにして下さい
+$file = fopen("data/data.txt","a");	// ファイル読み込み
+fwrite($file, $str."\r\n");//例） yamazaki , test@test.jp
+fclose($file);//確認のURL localhost/php01/post.php
 ?>
+
+
 <html>
 <head>
 <meta charset="utf-8">
 <title>POST（受信）</title>
 </head>
 <body>
-お名前：<?php echo $name; ?>
-EMAIL：<?php echo $mail; ?>
+<!-- お名前：<?php echo $name; ?>
+EMAIL：<?php echo $mail; ?> -->
 <?php 
    if ($flg == 0) {
  ?>
